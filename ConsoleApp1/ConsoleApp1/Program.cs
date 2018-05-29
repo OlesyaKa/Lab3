d930c3dab226976429d10cfa2736b6e7ca76180d
OlesyaKa <lesya-ledy@mail.ru>
2018-05-29 10:58:22 +0700

    New file1

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -3,13 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Reflection;
+using System.Diagnostics;
 
 namespace ConsoleApp1
+  
 {
     class Program
     {
         static void Main(string[] args)
         {
+            var assembly = Assembly.GetExecutingAssembly();
+            var fileVersionInfo = FileVersionInfo.GetVersionInfo(assembly.Location);
+            var version = fileVersionInfo.ProductVersion;
+            Console.WriteLine(version);
+            Console.Read();
+
         }
     }
 }
